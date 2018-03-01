22e230354c4fdbbedbe4b8bdb2ca4969b8717bfd
Luke Fisher <mail@lukefisher.me>
2018-03-01 18:15:08 +0000

    Add in unique id to ride

--- src/Ride.java
+++ src/Ride.java
@@ -1,15 +1,19 @@
 public class Ride  {
+    public int id;
+
     public Pair<Integer, Integer> start;
     public Pair<Integer, Integer> end;
 
     public int earliestStart;
     public int latestFinish;
 
-    public Ride(int startX, int startY, int endX, int endY, int earliest, int latest) {
+    public Ride(int id, int startX, int startY, int endX, int endY, int earliest, int latest) {
         this(new Pair<Integer, Integer>(startX, startY), new Pair<Integer, Integer>(endX, endY), earliest, latest);
     }
 
-    public Ride (Pair<Integer, Integer> start, Pair<Integer, Integer> end, int earliest, int latest) {
+    public Ride (int id, Pair<Integer, Integer> start, Pair<Integer, Integer> end, int earliest, int latest) {
+        this.id = id;
+
         this.start = start;
         this.end = end;
 
