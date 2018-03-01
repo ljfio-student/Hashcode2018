8ba749ee239adcc76c4c497abd50d1c688930fb9
Luke Fisher <mail@lukefisher.me>
2018-03-01 18:11:45 +0000

    Start reading in information

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(string[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) {
+        new Main(args[0], args[1]);
+    }
+
+    public Main(String inputFileName, String outputFileName) {
+
+    }
+
+    public void loadFile(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            // Get the information about grid/vehicles/bonus/steps
+            String[] information = reader.readLine().split(" ");
+
+            int rows = Integer.parseInt(information[0]);
+            int columns = Integer.parseInt(information[1]);
+            int vehicles = Integer.parseInt(information[2]);
+            int rides = Integer.parseInt(information[3]);
+            int bonus = Integer.parseInt(information[4]);
+            int steps = Integer.parseInt(information[5]);
+
+            // Read in the rides
+
+        } catch (IOException ex) {
+
+        }
     }
 }
